0bbbda9b0f05a15987516671dfc5f702aa4c7c7b
JoranOut <joranlol@gmail.com>
2023-09-12 17:00:07 +0200

    Initial commit

---
+++ soli-dev-blocks.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+  Plugin Name: Soli Development Plugin
+  Version: 1.0
+  Author: Joran Out
+*/
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+define( 'SOLI_DEV__BLOCKS_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'SOLI_DEV__BLOCKS_DIR_URL', plugin_dir_url( __FILE__ ) );
+
+
+class SoliDevBlocks {
+
+}
+
+$soliDevBlocks = new SoliDevBlocks();
+
+
